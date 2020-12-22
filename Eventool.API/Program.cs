c3c70c747499d8986998e5c288ae76c8408fd808
Azizjan Ayupov <litpulla@mail.ru>
2020-12-22 11:46:28 +0600

    API

---
+++ Eventool.API/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Eventool.Domain;
+
+class Program
+{
+   
+    static void Main(string[] args)
+    {
+        
+        var iPEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+
+        UdpClient udpServer = new UdpClient(11000);
+
+        while (true)
+        {
+
+            var data = udpServer.Receive(ref iPEndPoint); // listen on port 11000
+                                                        // Console.Write("receive data from " + remoteEP.ToString());
+            if (data != null)
+            {
+                udpServer.Send(data, data.Length, iPEndPoint); // reply back
+            }
+        }
+    }
+}
--- Eventool.Cmd/Program.cs
+++ Eventool.Cmd/Program.cs
@@ -4,24 +4,44 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        ApplicationDbContextFactory _contextFactory = new ApplicationDbContextFactory();
-        var cxt = _contextFactory.CreateDbContext();
-        var pt = new PlatformType
-        {
-            Name = Console.ReadLine()
-        };
-        cxt.PlatformTypes.Add(pt);
-        await cxt.SaveChangesAsync();
-        var list = await cxt.PlatformTypes.ToListAsync();
-        foreach (var item in list)
-        {
-            Console.WriteLine(item.Name);
-        }
+        var client = new UdpClient();
+        IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); // endpoint where server is listening
+        client.Connect(ep);
+
+        Console.WriteLine("Enter your message: ");
+        var msg = Console.ReadLine();
+        var data = Encoding.ASCII.GetBytes(msg);
+        // send data
+        client.Send(data, data.Length);
+
+        // then receive data
+        var receivedData = client.Receive(ref ep);
+        string someString = Encoding.ASCII.GetString(receivedData);
+
+        Console.Write("\nReceived data: " + someString);
+
+        Console.Read();
+        //ApplicationDbContextFactory _contextFactory = new ApplicationDbContextFactory();
+        //var cxt = _contextFactory.CreateDbContext();
+        //var pt = new PlatformType
+        //{
+        //    Name = Console.ReadLine()
+        //};
+        //cxt.PlatformTypes.Add(pt);
+        //await cxt.SaveChangesAsync();
+        //var list = await cxt.PlatformTypes.ToListAsync();
+        //foreach (var item in list)
+        //{
+        //    Console.WriteLine(item.Name);
+        //}
     }
 }
 
